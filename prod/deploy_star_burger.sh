df2dcef01ed4ece4e1181017a8a92fc9d6a0be86
AndreyShukatka <106096891+AndreyShukatka@users.noreply.github.com>
2023-07-23 14:12:13 +0300

    Update deploy_star_burger.sh

--- prod/deploy_star_burger.sh
+++ prod/deploy_star_burger.sh
@@ -1,11 +1,4 @@
 # #!/bin/bash
 cd ..
-set -Eeuo pipefail
 git pull
-docker compose up -d
-curl \
--H "X-Rollbar-Access-Token: $ROLLBAR_SERVER_TOKEN" \
--H "Content-Type: application/json" \
--X POST 'https://api.rollbar.com/api/1/deploy' \
--d '{"environment": "home_pk", "revision": "'"$REVISION"'", "rollbar_name": "Star-burger", "local_username": "'"$COMMIT_AUTHOR"'", "comment": "'"$COMMIT_COMMENT"'", "status": "succeeded"}'
-echo "Deploy completed successfully!"
+docker-compose up
